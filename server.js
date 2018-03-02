a14052c2db7552798e4198cca4dea654c6aa6fe5
Bruk Berhane <brukbrhane@gmail.com>
2018-03-02 16:50:00 +0300

    Finalized the basic functions of the REST API. Next will work on a web client to go with it inside the client folder (Will also be rewritten from scratch

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 var connection = MySQL.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'bruk',
     password: 'pass12',
     database: 'test'
@@ -18,6 +18,19 @@ connection.connect(function (req, rep){
 
 app.get("/helloworld", function (req, res){
     res.json({message: "Hello World!"});
+});
+
+app.post('/signup', (req, res) => {
+
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    connection.query("INSERT INTO users (username,password,email) VALUES ('"+username+"','"+password+"','"+email+"')", (err, reply, fields) => {
+        if (err) throw err;
+
+        res.json(reply);
+    });
 });
 
 app.get("/users", function(req, res){
@@ -37,5 +50,51 @@ app.post("/user", function(req, res){
         res.json(results);
     });
 });
+
+// API TESTING ROUTE
+app.get("/getuser", function(req, res){
+
+    res.send("<form action='/user' method='POST'> <input type='text' name='uid'><input type='submit'> </form>");
+});
+//TODO: DELETE ROUTE 
+
+app.post('/writeMessage', function(req, res){
+    
+    var post = {
+        message: request.body.message,
+        uid_fk: request.body.uid_fk
+    }
+
+    connection.query("INSERT INTO messages SET ?", post, function(err, result){
+        if (err) throw err;
+
+        res.json(result);
+    });
+
+});
+
+app.post('/messages', (req, res) => {
+    var uid = req.body.uid;
+
+    connection.query('SELECT * FROM messages WHERE uid_fk = "' + uid + '"', (error, results, fields) => {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+app.delete('/deleteMessage', (req, res) =>{
+    var uid = req.body.uid;
+    var mid = req.body.mid;
+
+    connection.query('DELETE FROM messages WHERE uid_fk = "' + uid + '" AND mid = "' + mid + '"', (error, result, fields) => {
+        if (error) throw error;
+
+        if (result.affectedRows){
+            res.json(true);
+        } else {
+            res.json(false);
+        }
+    })
+})
 
 app.listen(6969);
